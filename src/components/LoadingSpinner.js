90c1bd538e7f711214851168a4b108af5b8fa07c
Halina Balys <hbalys@gmail.com>
2021-01-14 16:34:00 +0100

    change invalid prop

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -3,7 +3,7 @@ import "./LoadingSpinner.css";
 
 const LoadingSpinner = () => {
   return (
-    <div class="lds-roller">
+    <div className="lds-roller">
       <div></div>
       <div></div>
       <div></div>
